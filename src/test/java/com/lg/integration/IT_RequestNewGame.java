f5afd8762aed8d1f885201fb74b8802eabb77efb
Lev Guzeev <lev.guzeev@gmail.com>
2017-10-18 23:36:55 +0200

    Running app in separate process on pre-integration-test phase instead of running in test - that also fixes tests for Mac OS due to exitFor() method not returning control

--- src/test/java/com/lg/integration/IT_RequestNewGame.java
+++ src/test/java/com/lg/integration/IT_RequestNewGame.java
@@ -7,7 +7,6 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
 
-import java.io.File;
 import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
@@ -18,19 +17,6 @@ public class IT_RequestNewGame {
     @Test
     public void When_command_request_Should_process() throws Exception {
         // Arrange
-
-        // Do this Before all tests
-        File jar = new File( "target/ardashir-jar-with-dependencies.jar" );
-
-        String[] execArgs = new String[3];
-        execArgs[0] = "java";
-        execArgs[1] = "-jar";
-        execArgs[2] = jar.getCanonicalPath();
-
-        Process p = Runtime.getRuntime().exec( execArgs );
-        p.waitFor();
-
-        // Make POST request
         CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpPost httpPost = new HttpPost("http://localhost:4567/command/RequestNewGame");
         httpPost.setEntity(new StringEntity("{\"gameId\":\"" + UUID.randomUUID().toString() + "\"}"));
@@ -41,9 +27,5 @@ public class IT_RequestNewGame {
         // Assert
         assertNotNull(response1.getEntity().getContent().toString());
         assertEquals(response1.getStatusLine().getStatusCode(), 200);
-
-        // Do Always
-        p.destroy();
-        p.exitValue();
     }
 }
