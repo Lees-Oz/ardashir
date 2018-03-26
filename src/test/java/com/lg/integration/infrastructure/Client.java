fb0c68f3c43cc53129f67d250d275b4a97efdf60
Lees-Oz <lev.guzeev@gmail.com>
2018-03-26 22:08:38 +0200

    Fixed retries of integration test http client

--- src/test/java/com/lg/integration/infrastructure/Client.java
+++ src/test/java/com/lg/integration/infrastructure/Client.java
@@ -20,15 +20,18 @@ import org.junit.Assert;
 
 import java.io.IOException;
 import java.time.temporal.ChronoUnit;
+import java.util.concurrent.Callable;
 
 public class Client {
 
-    private RetryConfig config = new RetryConfigBuilder()
-            .retryOnSpecificExceptions(Exception.class)
+    private RetryConfig retryConfig = new RetryConfigBuilder()
+            .retryOnAnyException()
             .withMaxNumberOfTries(10)
             .withDelayBetweenTries(500, ChronoUnit.MILLIS)
             .withExponentialBackoff()
             .build();
+
+    private CallExecutor executor = new CallExecutor(retryConfig).beforeNextTry(s -> System.out.println("Trying..."));
 
     private ResponseHandler<String> handler = new BasicResponseHandler();
     private SerializeJson serializer = new JsonSerializer();
@@ -38,7 +41,9 @@ public class Client {
         request.setEntity(new StringEntity(serializer.serialize(query)));
         HttpClient client = HttpClients.createDefault();
 
-        return (TResult) new CallExecutor(config).execute(() -> {
+        System.out.println("Query " + type.getSimpleName());
+
+        Callable<TResult> callable = () -> {
             HttpResponse response = client.execute(request);
             int statusCode = response.getStatusLine().getStatusCode();
             Assert.assertTrue(statusCode >= 200 && statusCode < 300);
@@ -46,13 +51,22 @@ public class Client {
             TResult r = (TResult) serializer.deserialize(body, type);
 
             if (test != null) {
-                test.test(r);
+                try {
+                    test.test(r);
+                } catch (AssertionError e) {
+                    throw new Exception("Query assertion failed: " + e);
+                }
             }
             return r;
-        }).getResult();
+        };
+
+        TResult result = (TResult) (executor.execute(callable)).getResult();
+        return result;
     }
 
     public void command(Command command) throws IOException {
+        System.out.println("Command " + command.getClass().getSimpleName());
+
         HttpPost request = new HttpPost(String.format("http://localhost:4567/command/%s", command.getClass().getSimpleName()));
         request.setEntity(new StringEntity(serializer.serialize(command)));
         HttpResponse response = HttpClients.createDefault().execute(request);
